d192d44442fab58f739c5a8de494b8703082eee2
Ifrago <ivan.f.a@hotmail.es>
2014-05-22 16:28:06 +0200

    Ultimo commit

---
+++ beeter-android/gen/edu/upc/eetac/dsa/ifrago/beeter/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.upc.eetac.dsa.ifrago.beeter.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
